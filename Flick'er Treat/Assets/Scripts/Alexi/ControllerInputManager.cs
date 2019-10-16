9f3214073eee7225968f054b29e4100cda8b891c
Alexi Most <alexibmost@gmail.com>
2019-10-16 16:33:11 -0700

    player moves

---
+++ Flick'er Treat/Assets/Scripts/Alexi/ControllerInputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// connects controllers to players
+/// </summary>
+public class ControllerInputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
