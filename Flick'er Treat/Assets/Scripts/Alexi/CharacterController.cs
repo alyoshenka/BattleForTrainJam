5812d9900f8219d547a5eeeba92a3a87f88283ff
Alexi Most <alexibmost@gmail.com>
2019-10-16 17:20:44 -0700

    rotation

--- Flick'er Treat/Assets/Scripts/Alexi/CharacterController.cs
+++ Flick'er Treat/Assets/Scripts/Alexi/CharacterController.cs
@@ -12,6 +12,9 @@ public class CharacterController : MonoBehaviour
     [Header("Controls")]
     [Tooltip("Movement force")] public float movementForce;
 
+    [Header("Cosmetics")]
+    [Tooltip("How fast the player rotates")] [Range(0.01f, 0.5f)] public float rotationLerp = 0.2f;
+
     [Tooltip("Rewired ID")]
     public int playerID;
 
@@ -21,6 +24,7 @@ public class CharacterController : MonoBehaviour
 
     // holders
     Vector2 movementInput, movementVector;
+    Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -29,15 +33,47 @@ public class CharacterController : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody>();
 
         movementInput = Vector2.zero;
+        targetRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementInput = new Vector2(player.GetAxis("MoveHorizontal"), player.GetAxis("MoveVertical"));
+        GetInput();
+        Move();
+        Rotate();
+    }
+
+    /// <summary>
+    /// get controller input
+    /// </summary>
+    void GetInput()
+    {
+        movementInput = new Vector2(player.GetAxis("MoveHorizontal"), player.GetAxis("MoveVertical")).normalized;
+    }
+
+    /// <summary>
+    /// apply the input
+    /// </summary>
+    void Move()
+    {
         if (movementInput.x > 0) { myRigidbody.AddForce(Vector3.right * movementForce); }
         else if (movementInput.x < 0) { myRigidbody.AddForce(Vector3.left * movementForce); }
         if (movementInput.y > 0) { myRigidbody.AddForce(Vector3.forward * movementForce); }
         else if (movementInput.y < 0) { myRigidbody.AddForce(Vector3.back * movementForce); }
+    }
+
+    /// <summary>
+    /// rotate with respect to velocity
+    /// </summary>
+    void Rotate()
+    {
+        float targetAngle = Mathf.Atan2(myRigidbody.velocity.x, myRigidbody.velocity.z) * Mathf.Rad2Deg;
+        float currentAngle = transform.rotation.eulerAngles.y;
+        if(currentAngle - targetAngle > 180) { currentAngle -= 360; }
+        float rotation = Mathf.Lerp(currentAngle, targetAngle, rotationLerp);
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+        Debug.Log(targetAngle + ", " + currentAngle);
     }
 }
