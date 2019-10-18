7abc91eac4980abbc948594df0969c804cfbe566
Alexi Most <alexibmost@gmail.com>
2019-10-18 15:28:30 -0700

    main rend

--- Flick'er Treat/Assets/Scripts/Alexi/CharacterController.cs
+++ Flick'er Treat/Assets/Scripts/Alexi/CharacterController.cs
@@ -96,10 +96,10 @@ public class CharacterController : MonoBehaviour
     /// </summary>
     void Move()
     {
-        if (movementInput.x > 0) { myRigidbody.AddForce(Vector3.right * movementForce); }
-        else if (movementInput.x < 0) { myRigidbody.AddForce(Vector3.left * movementForce); }
-        if (movementInput.y > 0) { myRigidbody.AddForce(Vector3.forward * movementForce); }
-        else if (movementInput.y < 0) { myRigidbody.AddForce(Vector3.back * movementForce); }
+        if (movementInput.x > 0) { myRigidbody.AddForce(Vector3.right * movementForce * Time.deltaTime); }
+        else if (movementInput.x < 0) { myRigidbody.AddForce(Vector3.left * movementForce * Time.deltaTime); }
+        if (movementInput.y > 0) { myRigidbody.AddForce(Vector3.forward * movementForce * Time.deltaTime); }
+        else if (movementInput.y < 0) { myRigidbody.AddForce(Vector3.back * movementForce * Time.deltaTime); }
     }
 
     /// <summary>
